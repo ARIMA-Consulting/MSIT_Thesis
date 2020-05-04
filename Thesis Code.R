ab1e4877ee35a91d906c4a64fb000297b74c38bb
Tyler Harris <tylerharris294@gmail.com>
2020-05-03 22:21:46 -0400

    Next Round, added ETS and template for ARIMA
    updated code, added ETS models AAN and ANN to compare forecast against ARIMA. Also got format for ARIMA models to change all p, d, and q values. Forecasts work, need to work on dates and subsetting test/train data

--- Thesis Code.R
+++ Thesis Code.R
@@ -3,10 +3,16 @@ library(quantmod)
 library(tseries)
 library(xts)
 library(forecast)
+library(tibble)
+library(stinepack)
+library(ggplot2)
+
+# Set Seed
+set.seed(1337)
 
 # Gets NASDAQ 100 Technology Sector Index (^NDXT)
 tickers <- c("^NDXT")
-getSymbols(tickers, src = "yahoo", from = '2012-01-01', to = '2020-05-01')
+getSymbols(tickers, src = "yahoo", from = '2006-02-21', to = '2020-03-31')
 
 # Head of NDXT
 head(NDXT)
@@ -18,7 +24,12 @@ summary(NDXT)
 plot(NDXT$NDXT.Close)
 
 # Create Univariate Time Series
-NDXTclose <- ts(NDXT$NDXT.Close)
+NDXTclose <- na.interp(ts(NDXT$NDXT.Close))
+NDXTclose <- na.interp(NDXTclose)
+
+# Check Normality Assumption
+qqnorm(fit$residuals)
+qqline(fit$residuals)
 
 # Auto ARIMA Test
 # Create fit model
@@ -28,7 +39,16 @@ fit <- auto.arima(NDXTclose)
 fit
 
 # Create forecast
-forecastAutoARIMA <- forecast(fit, 20)
+forecastAutoARIMA <- forecast(fit, 30)
+
+# Fix Columns to Date
+
+#TestStartDate <- as.Date("2020-04-01")
+#TestEndDate <- as.Date("2020-04-30")
+#TestDateRange <- c(seq(TestStartDate, TestEndDate, by = "day"))
+#rownames(forecastAutoARIMA) <- TestDateRange
+#forecastAutoARIMA$Date <- TestDateRange
+#forecastAutoARIMA <- data.frame(forecastAutoARIMA)
 
 # Quick plot of Auto ARIMA model forecast
 plot(forecastAutoARIMA)
@@ -36,15 +56,27 @@ plot(forecastAutoARIMA)
 # Check Accuracy of Auto ARIMA model
 accuracy(fit)
 
-# Check Normality Assumption
-qqnorm(fit$residuals)
-qqline(fit$residuals)
-
 # Ljung-Box Test
 Box.test(fit$residuals, type = "Ljung-Box")
 
-# Seasonal Decomposition
+# Different ARIMA
+ARIMA212 <- arima(NDXTclose, order = c(2,1,2))
+ARIMA212
+ARIMA212forecast <- forecast(ARIMA212, 30)
+plot(ARIMA212forecast)
 
+# ETS Model Predictions
+demofit <- ets(NDXTclose, model = "AAN")
+demofitForecast <- forecast(demofit, 30)
+demofit
+
+demofit2 <- ets(NDXTclose, model = "ANN")
+demofit2Forecast <- forecast(demofit2, 30)
+demofit2
+
+# Plot ETS Predictions
+plot(demofitForecast)
+plot(demofit2Forecast)
 
 
 
